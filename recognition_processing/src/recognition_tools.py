75f1cafce74a0790ddd5ecaa6ee331189706c420
HappyTatsuhito <c6100166@planet.kanazawa-it.ac.jp>
2021-08-20 16:14:19 +0900

    Change: object_recognizerをtoolsとaction_serverに分割

--- recognition_processing/src/recognition_tools.py
+++ recognition_processing/src/recognition_tools.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
-import sys
 import time
-import math
 import numpy
 import rospy
 import rosparam
-import actionlib
 from geometry_msgs.msg import Twist, Point
 from darknet_ros_msgs.msg import BoundingBoxes
 # -- Custom Message --
 from happymimi_recognition_msgs.srv import RecognizeFind, RecognizeCount, RecognizeLocalize, PositionEstimator
-# -- Action msg --
-from happymimi_recognition_msgs.msg import *
 
 class MimiControl(object):
     def __init__(self):
@@ -72,12 +67,12 @@ class CallDetector(object):
         rospy.loginfo(self.object_centroid)
         
 
-class RecognizeTools(object):
+class RecognitionTools(object):
     def __init__(self):
         rospy.Subscriber('/darknet_ros/bounding_boxes',BoundingBoxes,self.boundingBoxCB)
-        rospy.Service('/recognize/find',RecognizeFind,self.findObject)
-        rospy.Service('/recognize/count',RecognizeCount,self.countObject)
-        rospy.Service('/recognize/localize',RecognizeLocalize,self.localizeObject)
+        rospy.Service('/recognition/find',RecognizeFind,self.findObject)
+        rospy.Service('/recognition/count',RecognizeCount,self.countObject)
+        rospy.Service('/recognition/localize',RecognizeLocalize,self.localizeObject)
 
         self.object_dict = rosparam.get_param('/object_dict')
         self.bbox = []
@@ -188,73 +183,9 @@ class RecognizeTools(object):
         Detector.detectorService(center_x, center_y)
         object_centroid = Detector.object_centroid
         return object_centroid
-        
-        
-class RecognizeAction(object):
-    def __init__(self):
-        self.act = actionlib.SimpleActionServer('/manipulation/localize',
-                                                RecognitionProcessingAction,
-                                                execute_cb = self.actionMain,
-                                                auto_start = False)
-        self.act.register_preempt_callback(self.actionPreempt)
-
-        self.preempt_flg = False
-        self.act.start()
-
-        self.recognize_tools = RecognizeTools()
-        self.recognize_tools.initializeBBox()
-
-    def actionPreempt(self):
-        rospy.loginfo('preempt callback')
-        self.act.set_preempted(text = 'message for preempt')
-        self.preempt_flg = True
-
-    def actionMain(self, goal):
-        target_name = goal.recognize_goal
-        rospy.loginfo('start action >> recognize %s'%(target_name))
-        action_feedback = RecognitionProcessingFeedback()
-        action_result = RecognitionProcessingResult()
-        mimi_control = MimiControl()
-        move_count = 0
-        while not rospy.is_shutdown():
-            bb = self.recognize_tools.bbox
-            object_count, _ = self.recognize_tools.countObject(target_name, bb)
-            exist_flg = bool(object_count)
-            if exist_flg:
-                object_centroid = self.recognize_tools.localizeObject(target_name, bb)
-                if not math.isnan(object_centroid.x):# 物体が正面になるように回転する処理
-                    object_angle = math.atan2(object_centroid.y, object_centroid.x)/math.pi*180
-                    if abs(object_angle) < 4.5:
-                        # success
-                        rospy.loginfo('Succeeded')
-                        action_result.recognize_result = object_centroid
-                        self.act.set_succeeded(action_result)
-                        break
-                    else:
-                        # retry
-                        rospy.loginfo('There is not object in front.')
-                        object_angle *= 1.5 # kobukiが重いので
-                        if abs(object_angle) < 10: object_angle=object_angle/abs(object_angle)*10
-                        mimi_control.angleRotation(object_angle)
-                        rospy.sleep(4.0)
-                else:
-                    #前後進
-                    move_count += 1
-                    move_range = -0.8*(((move_count)%4)/2)+0.4
-                    mimi_control.moveBase(move_range)
-                    exist_flg = False
-            else:
-                find_flg = self.recognize_tools.findObject(target_name)
-                exist_flg = find_flg
-            action_feedback.recognize_feedback = exist_flg
-            self.act.publish_feedback(action_feedback)
-            rospy.sleep(1.0) #preemptのズレ調整用
-            if self.preempt_flg:
-                self.preempt_flg = False
-                break
 
 
 if __name__ == '__main__':
-    rospy.init_node('object_recognizer')
-    recognize_action = RecognizeAction()
+    rospy.init_node('recognition_tools')
+    recognition_tools = RecognitionTools()
     rospy.spin()
