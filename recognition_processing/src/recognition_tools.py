aba7b2f139db53cb93fc5135b5e4d76f0863f4e5
HappyTatsuhito <c6100166@planet.kanazawa-it.ac.jp>
2021-10-28 20:20:28 +0900

    Update: Findの回転速度を上昇

--- recognition_processing/src/recognition_tools.py
+++ recognition_processing/src/recognition_tools.py
@@ -153,7 +153,7 @@ class RecognitionTools(object):
             loop_count += 1
 
             rotation_angle = float(45 - (((loop_count)%4)/2) * 90)
-            base_control.rotateAngle(rotation_angle)
+            base_control.rotateAngle(rotation_angle, 0.3)
             rospy.sleep(3.0)
 
             bbox_list = self.createBboxList(RecognitionTools.bbox)
