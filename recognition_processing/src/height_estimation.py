0f35ffcf86af54cc3bad4295315c3dd54f349066
HappyTatsuhito <c6100166@planet.kanazawa-it.ac.jp>
2021-11-17 19:26:52 +0900

    Update: frameでやってみた

--- recognition_processing/src/height_estimation.py
+++ recognition_processing/src/height_estimation.py
@@ -2,20 +2,22 @@
 # -*- coding: utf-8 -*
 
 import rospy
-from ros_openpose.msg import AltMarkerArray
-from happymimi_msgs.srv import SetFloat
+from ros_openpose.msg import AltMarkerArray, Frame
+#from happymimi_msgs.srv import SetFloat
 from happymimi_recognition_msgs.srv import RecognitionLocalize, RecognitionLocalizeRequest
 
 class HeightEstimation(object):
     def __init__(self):
-        rospy.Service('/height_estimation', SetFloat, self.main)
-        rospy.Subscriber('/visualization', AltMarkerArray, self.openPoseCB)
+        #rospy.Service('/height_estimation', SetFloat, self.main)
+        #rospy.Subscriber('/visualization', AltMarkerArray, self.openPoseCB)
+        rospy.Subscriber('/frame', Frame, self.openPoseCB)
 
-        self.pose_res = AltMarkerArray()
+        #self.pose_res = AltMarkerArray()
+        #self.pose_res = Frame()
 
     def openPoseCB(self, res):
         self.pose_res = res
-        print res
+        print res.persons[0].bodyParts[0]
 
     def main(self):
         self.pose_res.markers[0]
@@ -28,7 +30,7 @@ class HeightEstimation(object):
 
 if __name__ == '__main__':
     rospy.init_node('height_estimation')
-    
+
     rospy.sleep(0.5)
     HeightEstimation()
     rospy.spin()
