c11b0b9c6fa753bbe760f1fee19916885f0e38ea
HappyTatsuhito <c6100166@planet.kanazawa-it.ac.jp>
2021-11-19 03:26:34 +0900

    Update: x, yの代入タイミング変更

--- person_feature_extraction/src/height_estimation.py
+++ person_feature_extraction/src/height_estimation.py
@@ -4,7 +4,7 @@
 import rospy
 from ros_openpose.msg import AltMarkerArray, Frame
 from std_msgs.msg import Float64
-from happymimi_msgs.srv import SetFloat, SetFloatResponce
+from happymimi_msgs.srv import SetFloat, SetFloatResponse
 from happymimi_recognition_msgs.srv import PositionEstimator, PositionEstimatorRequest
 
 class HeightEstimation(object):
@@ -20,28 +20,28 @@ class HeightEstimation(object):
         self.pose_res = res
 
     def main(self, _):
-        height = SetFloatResponce(data=-1)
+        height = SetFloatResponse(data=-1)
         self.head_pub.publish(-25.0)
         rospy.sleep(1.0)
 
         pose = self.pose_res
         if len(pose.persons)==0: return height
 
-        center_x = pose.persons[0].bodyParts[0].pixel.x
-        center_y = pose.persons[0].bodyParts[0].pixel.y
+        center_x = pose.persons[0].bodyParts[0].pixel.y
+        center_y = pose.persons[0].bodyParts[0].pixel.x
         if center_x==0 and center_y==0:
-            center_x = pose.persons[0].bodyParts[15].pixel.x
-            center_y = pose.persons[0].bodyParts[15].pixel.y
+            center_x = pose.persons[0].bodyParts[15].pixel.y
+            center_y = pose.persons[0].bodyParts[15].pixel.x
             if center_x==0 and center_y==0:
-                center_x = pose.persons[0].bodyParts[16].pixel.x
-                center_y = pose.persons[0].bodyParts[16].pixel.y
+                center_x = pose.persons[0].bodyParts[16].pixel.y
+                center_y = pose.persons[0].bodyParts[16].pixel.x
                 if center_x==0 and center_y==0:
                     return height
 
         rospy.wait_for_service('/detect/depth')
         p_e_req = PositionEstimatorRequest()
-        p_e_req.center_x = int(center_y)
-        p_e_req.center_y = int(center_x)
+        p_e_req.center_x = int(center_x)
+        p_e_req.center_y = int(center_y)
         p_e_res = self.position_estimate(p_e_req).point
 
         height.data = p_e_res.z*100 + 15
@@ -49,5 +49,5 @@ class HeightEstimation(object):
 
 if __name__ == '__main__':
     rospy.init_node('height_estimation')
-    h_e = HeightEstimation()
+    height_estimation = HeightEstimation()
     rospy.spin()
