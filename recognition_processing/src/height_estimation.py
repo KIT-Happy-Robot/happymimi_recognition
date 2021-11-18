26b25373194fa29084b7a6c6185b51581749d15b
HappyTatsuhito <c6100166@planet.kanazawa-it.ac.jp>
2021-11-18 13:51:53 +0900

    Update: headを追加

--- recognition_processing/src/height_estimation.py
+++ recognition_processing/src/height_estimation.py
@@ -3,6 +3,7 @@
 
 import rospy
 from ros_openpose.msg import AltMarkerArray, Frame
+from std_msgs.msg import Float64
 from happymimi_msgs.srv import SetFloat
 from happymimi_recognition_msgs.srv import PositionEstimator, PositionEstimatorRequest
 
@@ -12,6 +13,7 @@ class HeightEstimation(object):
         #rospy.Subscriber('/visualization', AltMarkerArray, self.openPoseCB)
         rospy.Subscriber('/frame', Frame, self.openPoseCB)
         self.position_estimate = rospy.ServiceProxy('/detect/depth', PositionEstimator)
+        self.head_pub = rospy.Publisher('/servo/head', Float64, queue_size=1)
 
         self.pose_res = Frame()
 
@@ -19,13 +21,18 @@ class HeightEstimation(object):
         self.pose_res = res
 
     def main(self):
+        '''
+        self.head_pub.publish(-25.0)
+        rospy.sleep(1.0)
+        '''
+
         pose = self.pose_res
         if len(pose.persons)==0: return -1.0
 
         print pose.persons[0].bodyParts[0].pixel
         center_x = pose.persons[0].bodyParts[0].pixel.x
         center_y = pose.persons[0].bodyParts[0].pixel.y
-        print center_x, center_y
+        print center_y, center_x
         if center_x==0 and center_y==0: return -1.0
 
         rospy.wait_for_service('/detect/depth')
@@ -34,7 +41,7 @@ class HeightEstimation(object):
         p_e_req.center_y = int(center_x)
         p_e_res = self.position_estimate(p_e_req).point
 
-        print 'z:', p_e_res.z
+        print 'res:', p_e_res
         height = p_e_res.z*100 + 20
         return height
 
