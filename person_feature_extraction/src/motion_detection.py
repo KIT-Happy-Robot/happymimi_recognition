5c16e65b8bd58547d3c453b5360d8ef3f742a3a4
Ayu <108796833+DaningenAyu@users.noreply.github.com>
2023-02-22 10:41:10 +0900

    update motion_ditection 2/22

--- person_feature_extraction/src/motion_detection.py
+++ person_feature_extraction/src/motion_detection.py
@@ -7,59 +7,57 @@ import sys
 
 class ditection():
     def __init__(self):
-        motion_dt = rospy.ServiceProxy('motion_dt',MotionBool,self.motion)
         self.cap = cv2.VideoCapture(0)  #カメラの設定（パソコン内蔵カメラは「0」）
         self.before = None              #画像の比較（1つ前の画像）
-        self.count = 0                  #テスト用カウント変数
         self.colour = (0,0,255)         #線の色（赤）
 
-    #main関数
-    def motion(self):
+    def motion_detection_server(self):
+        rospy.init_node('motion_deteciton_sever')
+        s = rospy.Service('motion_dt',MotionBool,self.motion)
+        print('Ready to motion detection')
+        rospy.spin()
+    
+    def motion(self,req):
         res = MotionBoolResponse()
-        rospy.wait_for_service('motion_dt')
-        try:
-            while True:
-                ret,frame = self.cap.read()
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        count = 0
+        while True:
+            ret,frame = self.cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 
+            if self.before is None:
+                self.before = gray.astype('float')
+                continue
 
-                if self.before is None:
-                    self.before = gray.astype('float')
+            #現在のフレームと移動平均との差を計算
+            cv2.accumulateWeighted(gray, self.before, 0.88)
+            frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(self.before))
+            #frameDeltaの画像を２値化
+            thresh = cv2.threshold(frameDelta, 3 , 255, cv2.THRESH_BINARY)[1]
+            #輪郭のデータを得る
+            contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+                
+            #差分があった点を画面に描く
+            for target in contours:
+                x,y,w,h = cv2.boundingRect(target) 
+                if w < 100:
+                    res.result = False
                     continue
-
-                #現在のフレームと移動平均との差を計算
-                cv2.accumulateWeighted(gray, self.before, 0.88)
-                frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(self.before))
-                #frameDeltaの画像を２値化
-                thresh = cv2.threshold(frameDelta, 3 , 255, cv2.THRESH_BINARY)[1]
-                #輪郭のデータを得る
-                contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+                else:
+                    res.result = True
+                cv2.rectangle(frame,(x,y),(x+w,y+h),self.colour,2)
+                count += 1
                 
-                #差分があった点を画面に描く
-                for target in contours:
-                    x,y,w,h = cv2.boundingRect(target)
-                    if w < 100:
-                        continue
-                    cv2.rectangle(frame,(x,y),(x+w,y+h),self.colour,2)
-                    self.count += 1
-                    if count > 20:
-                        print('検知しました{}'.format(self.count))
-                        return res.result == True
+                
+                print('検知しました{}'.format(count))
+                if count > 20:
+                    print('result:{}'.format(res.result))
+                    cv2.destroyAllWindows()
+                    return res.result
 
-
-                #ウィンドウで表示
-                cv2.imshow('target_frame',frame)
-                #Enterキーが押されたらループを抜ける
-                if cv2.waitKey(1) == 13:
-                    break
-
-            #ウィンドウを破棄
-            cv2.destroyAllWindows()
-
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
-
+            #ウィンドウで表示
+            cv2.imshow('target_frame',frame)
+            cv2.waitKey(1) 
 
 if __name__ == '__main__':
     d = ditection()
-    d.motion()
+    d.motion_detection_server()
