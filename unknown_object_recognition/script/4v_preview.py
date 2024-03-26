cdc1c8b9f33569d24fa05a3795ed4877909bc375
Takuchan <takuchanapp@gmail.com>
2024-03-26 13:48:25 +0900

    object extract tuiki

--- unknown_object_recognition/script/4v_preview.py
+++ unknown_object_recognition/script/4v_preview.py
@@ -123,6 +123,6 @@ payload = {
   "max_tokens": 300
 }
 response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-print(response.json())
+#print(response.json())
 
 print(response.json()["choices"][0]["message"]["content"])
