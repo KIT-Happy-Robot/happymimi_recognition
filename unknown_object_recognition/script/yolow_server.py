d0e9a21e5f7d9b846d42c03f38e08904a5b21073
happykoya <c1103809@planet.kanazawa-it.ac.jp>
2024-03-26 20:49:47 +0900

    💫FIX:gpsr用に改良

--- unknown_object_recognition/script/yolow_server.py
+++ unknown_object_recognition/script/yolow_server.py
@@ -3,13 +3,16 @@
 
 import cv2
 import sys
+import numpy
 from ultralytics import YOLOWorld
 import rospy
 import roslib
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 from happymimi_msgs.srv import SetStr
+from happymimi_recognition_msgs.srv import PositionEstimator, PositionEstimatorRequest
 import yaml
 
 # Initialize the model with pre-trained weights
@@ -24,13 +27,18 @@ sys.path.insert(0,model_path)
 param_yaml_name = "uor_server_config.yaml"
 point_data_yaml_name = "uor_object_location.yaml"
 
+recognition_path = roslib.packages.get_pkg_dir("recognition_processing") + '/src/'
+sys.path.insert(1,recognition_path)
+from recognition_tools import CallDetector
+
 with open(yaml_path+param_yaml_name, 'r') as file:
     yaml_data = yaml.safe_load(file)
 
 class YoloWorld_Server():
     def __init__(self):
         self.bridge = CvBridge()
-        rospy.Subscriber('/camera/color/image_raw', Image, self.yolo_topic)
+        #rospy.Subscriber('/camera/color/image_raw', Image, self.yolo_topic)
+        rospy.Subscriber('/usb_cam/image_raw', Image, self.yolo_topic)
         rospy.Service('/uor/yolo_server', SetStr,self.yolo_main)
         
         rospy.set_param('/uor/config', yaml_data)
@@ -70,8 +78,18 @@ class YoloWorld_Server():
         
         self.model = YOLOWorld(model=self.model_name)
         self.model.set_classes(self.model_class)
-       
+    
+    def LocalizeObjectWorld(self,x_point, y_point):
+        Detector = CallDetector()
+        Detector.detectorService(x_point, y_point)
+        return Detector.object_centroid
+    
     def yolo_main(self, _):
+        
+        centroid = Point()
+        centroid.point.x = numpy.nan
+        centroid.point.y = numpy.nan
+        centroid.point.z = numpy.nan
         # JPEG形式のバイナリデータをファイルに保存する場合
         with open("output.jpg", "wb") as f:
             f.write(self.jpeg_data.tobytes())
@@ -81,11 +99,15 @@ class YoloWorld_Server():
         if results[0].boxes:
             for j in range(len(results[0].boxes)):
                 xmin,ymin,xmax,ymax =[int(i) for i in results[0].boxes.xyxy[j]]
-                self.point_data.append([int(xmax - xmin), int(ymax - ymin)])
+                x_point = xmax - xmin
+                y_point = ymax - ymin
+                centroid = self.LocalizeObjectWorld(x_point=x_point, y_point=y_point)
+                self.point_data.append([centroid.x, centroid.y, centroid.z])
             
-            id = int(results[0].boxes.cls)
-            self.point_data_dict = {results[0].names[id] + f"_{i}": coord for i, coord in enumerate(self.point_data)}
-            self.point_data = []
+                id = int(results[0].boxes[j].cls)
+                for i,coord in enumerate(self.point_data):
+                    key = results[0].names[id]
+                    self.point_data_dict.setdefault(key,coord)
         
         else:
             self.point_data_dict = {}
@@ -97,8 +119,25 @@ class YoloWorld_Server():
             yaml.dump(self.point_data_dict, file)
         
         return "True"
-        
-
+    
+    def load_yaml_to(self,file_path):
+        point = Point()
+        with open(file_path, 'r') as file:
+            yaml_data = yaml.safe_load(file)
+            if yaml_data:
+                for key, value in yaml_data.items():
+                    if isinstance(value, list) and len(value) == 3:
+                        point.x = value[0]
+                        point.y = value[1]
+                        point.z = value[2]
+                        return point
+                    else:
+                        rospy.logwarn("Invalid data format for key %s", key)
+                        point.x = numpy.nan
+                        point.y = numpy.nan
+                        point.z = numpy.nan
+                        return point
+    
 if __name__ == '__main__':
     rospy.init_node("yolo_topic")
     YoloWorld_Server()
